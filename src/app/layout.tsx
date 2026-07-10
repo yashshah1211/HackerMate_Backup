e2ff0e170a3cdbff5bdc0b321b03bdd5ccaa747c
yashshah1211 <yashshah7117@gmail.com>
2026-07-10 20:25:02 +0530

    feat: add Vercel Analytics and implement base layout structure with metadata and global providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
