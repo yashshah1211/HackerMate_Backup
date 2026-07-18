04679e312e6402fc29080c608e657198305c1834
yashshah1211 <yashshah7117@gmail.com>
2026-07-18 23:35:50 +0530

    Branding: Configure Next.js rewrite proxy for client-side Supabase auth to support custom domain branding on OAuth for free

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "192.168.0.100",
   ],
+  async rewrites() {
+    return [
+      {
+        source: "/temp-auth/v1/:path*",
+        destination: "https://rhryjrbebfrrfhtyyzbs.supabase.co/auth/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
